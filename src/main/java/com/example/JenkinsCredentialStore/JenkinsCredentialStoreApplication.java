c5d068eb109c3106800a462a4d155efff06d2159
Kishore <kishore.ssn.ece@gmail.com>
2025-01-06 16:43:17 -0500

    second commit

---
+++ src/main/java/com/example/JenkinsCredentialStore/JenkinsCredentialStoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.JenkinsCredentialStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsCredentialStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsCredentialStoreApplication.class, args);
+	}
+
+}
